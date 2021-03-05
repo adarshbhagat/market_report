107376787bfe292554053b0e05d603bf33d2c05a
Adarsh Sandhu <59194987+adarshbhagat@users.noreply.github.com>
2021-03-05 14:07:44 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -72,7 +72,8 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 df = pd.read_html(str(soup))
 df=df[1]
-st.write(df[df['5-Day Avg Del %']>50])
+df=df[df['5-Day Avg Del %']>50]
+st.write(df)
 
 url=get_url()
 s=requests.get(url).content
