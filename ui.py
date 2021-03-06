888250a68cc403a445188758978527d402e6fdf6
Adarsh Sandhu <59194987+adarshbhagat@users.noreply.github.com>
2021-03-06 18:05:33 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,4 +1,3 @@
-#importing all libraries
 import pandas as pd
 import io
 import getpass
@@ -12,6 +11,19 @@ import matplotlib
 import matplotlib.pyplot as plt
 st.markdown('<style>body{background-color: #8fa5ff;}</style>',unsafe_allow_html=True)
 
+li = ['TOP GAINERS IN NIFTY-200','TOP LOOSERS IN NIFTY-200','HIGH DELIVERY PERCENTAGE','LOW DELIVERY PERCENTAGE','STOCKS DRAGGING NIFTY UP','STOCKS DRAGGING NIFTY DOWN','TOP GAINER SECTORS','TOP LOOSER SECTORS']
+sel = st.selectbox('Select any one', li)
+
+def plot_bar(df,col1,col2,x_label,y_label):
+    st.title(sel)
+    fig = plt.figure(figsize = (10, 6))
+    plt.bar(df[col1], df[col2], color ='royalblue', width = 0.4)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    fig.autofmt_xdate()
+    st.pyplot(plt)
+    
+## For NIFTY-200
 url = 'https://www.moneycontrol.com/stocks/marketstats/indexcomp.php?optex=NSE&opttopic=indexcomp&index=49'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -21,36 +33,13 @@ page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 result = soup.find(class_='tbldata14 bdrtpg')
 df = pd.read_html(str(result))[0]
+df['Company Name']  = df['Company Name'].str[:-34]
 df.sort_values(by=['%Chg'], inplace=True, ascending=False)
 gainers=df.head()# Contains top gainers from NIFTY 200
 df.sort_values(by=['%Chg'], inplace=True, ascending=True)
 loosers = df.head()# contains top loosers from NIFTY 200
 
-st.title('NIFTY 200 Performance')
-
-fig = plt.figure(figsize = (10, 5)) 
-  
-# creating the bar plot 
-plt.bar(gainers['Company Name'].str[:-34], gainers['%Chg'], color ='royalblue',  
-        width = 0.4) 
-  
-plt.xlabel("Company Name")
-plt.ylabel("% Change")
-plt.title("Top Gainers in NIFTY-200")
-fig.autofmt_xdate()
-st.pyplot(plt)
-
-fig = plt.figure(figsize = (10, 5))
-  
-# creating the bar plot
-plt.bar(loosers['Company Name'].str[:-34], loosers['%Chg'], color ='royalblue',  
-        width = 0.4) 
-  
-plt.xlabel("Company Name") 
-plt.ylabel("% Change") 
-plt.title("Top Loosers in NIFTY-200")
-fig.autofmt_xdate()
-st.pyplot(plt)
+#Sector wise
 
 URL = 'https://www.zeebiz.com/market/sectors-nse'
 page = requests.get(URL)
@@ -60,64 +49,44 @@ df = df[0]
 df['Change %'] = df['Change %'].str[:-2]
 df["Change %"] = pd.to_numeric(df["Change %"])
 df.sort_values(by=['Change %'], inplace=True,ascending=False)
-df1 = df.head()
-df2 = df.tail()
+s1 = df.head()
+s2 = df.tail()
 
-fig = plt.figure(figsize = (10, 5)) 
-st.title("Sector Performance")
-plt.bar(df1['Sectors'], df1['Change %'], color ='royalblue',  
-        width = 0.4) 
-
-plt.xlabel("Sector") 
-plt.ylabel("% Change") 
-plt.title("Top performing sector")
-fig.autofmt_xdate()
-st.pyplot(plt)
-
-fig = plt.figure(figsize = (10, 5)) 
-plt.bar(df2['Sectors'], df2['Change %'], color ='royalblue',  
-        width = 0.4) 
-  
-plt.xlabel("Sector") 
-plt.ylabel("% Change") 
-plt.title("Top performing sector")
-fig.autofmt_xdate()
-st.pyplot(plt)
-
-
-URL = 'https://www.moneycontrol.com/india/stockmarket/stock-deliverables/marketstatistics/indices/cnx-200.html'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-df = pd.read_html(str(soup))
-df=df[1]
-st.title("High Delivery percentage(NIFTY-200)")
-st.write(df[df['5-Day Avg Del %']>60].reset_index(drop=True))
-st.title("Low Delivery percentage(NIFTY-200)")
-st.write(df[df['5-Day Avg Del %']<20].reset_index(drop=True))
-
+#Stocks dragging NIFTY
 
 URL = 'https://www.moneycontrol.com/indian-indices/nifty-50-9.html'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 result = soup.find(id='indi_contribute')
 df = pd.read_html(str(result))# stocks dragging index(up and don both)
-d1=df[0]# stocks dragging nifty up
-d2=df[1]# stocks dragging nifty down
+dn1=df[0]# stocks dragging nifty up
+dn2=df[1]# stocks dragging nifty dow
 
-fig = plt.figure(figsize = (10, 5))
-plt.bar(d1['Stock Name'], d1['Contribution'], color ='royalblue',  width = 0.4) 
-st.title("Stocks dragging NIFTY up")
-plt.xlabel("Stocks Name") 
-plt.ylabel("% Change") 
-plt.title("Stock influencing NIFTY")
-fig.autofmt_xdate()
-st.pyplot(plt)
+#Delivery Percentage
+URL = 'https://www.moneycontrol.com/india/stockmarket/stock-deliverables/marketstatistics/indices/cnx-200.html'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+df = pd.read_html(str(soup))
+df=df[1]
+dp1 = df[df['5-Day Avg Del %']>60].reset_index(drop=True)
+dp2 = df[df['5-Day Avg Del %']<20].reset_index(drop=True)
 
-fig = plt.figure(figsize = (10, 5))
-plt.bar(d2['Stock Name'], d2['Contribution'], color ='royalblue',  width = 0.4) 
-st.title("Stocks dragging NIFTY down")
-plt.xlabel("Stocks Name") 
-plt.ylabel("% Change") 
-plt.title("Stock influencing NIFTY")
-fig.autofmt_xdate()
-st.pyplot(plt)
+    
+if sel == 'TOP GAINERS IN NIFTY-200':
+    plot_bar(gainers,'Company Name','%Chg','Gainers','Change %')
+elif sel == 'TOP LOOSERS IN NIFTY-200':
+    plot_bar(loosers,'Company Name','%Chg','Loosers','Change %')
+elif sel == 'TOP GAINER SECTORS':
+    plot_bar(s1,'Sectors','Change %', 'Sector', 'Change %')
+elif sel == 'TOP LOOSER SECTORS':
+    plot_bar(s2,'Sectors','Change %', 'Sector', 'Change %')
+elif sel == 'STOCKS DRAGGING NIFTY UP':
+    plot_bar(dn1,'Stock Name','Contribution', 'Stock Name', 'Contribution')
+elif sel == 'STOCKS DRAGGING NIFTY DOWN':
+    plot_bar(dn2,'Stock Name','Contribution', 'Stock Name', 'Contribution')
+elif sel == 'HIGH DELIVERY PERCENTAGE':
+    st.write(dp1[['Company Name', 'Chg %', 'Dely %', '5-Day Avg Del %']])
+else:
+    st.write(dp2[['Company Name', 'Chg %', 'Dely %', '5-Day Avg Del %']])
+    
+   
